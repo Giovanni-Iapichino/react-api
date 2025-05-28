fe85094571935ee9bb593f3543fd8df9b26f83ca
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-28 15:15:15 +0200

    milestone-1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,13 @@
+import axios from "axios";
+
 export default function Main() {
+  const apiBaseUrl = "https://lanciweb.github.io/demo/api/actresses/";
+
+  axios.get(`${apiBaseUrl}`).then((res) => {
+    console.log(res.data);
+  });
+
+  const { results } = res.data;
+
   return;
 }
