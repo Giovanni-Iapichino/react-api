36a0131678cec051d5533a3e8fd7ce4438c66641
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-28 17:05:17 +0200

    add-bonus-1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,25 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export default function Main() {
-  const apiBaseUrl = "https://lanciweb.github.io/demo/api/actresses/";
+  const apiBaseUrlActresses = "https://lanciweb.github.io/demo/api/actresses/";
   const [actresses, setActresses] = useState([]);
 
+  const apiBaseUrlActors = "https://lanciweb.github.io/demo/api/actors/";
+  const [actors, setActors] = useState([]);
+
   useEffect(() => {
-    axios.get(`${apiBaseUrl}`).then((res) => {
+    axios.get(`${apiBaseUrlActresses}`).then((res) => {
       console.log(res.data);
       const results = res.data;
       setActresses(results);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get(`${apiBaseUrlActors}`).then((res) => {
+      console.log(res.data);
+      const results = res.data;
+      setActors(results);
     });
   }, []);
 
@@ -43,6 +54,42 @@ export default function Main() {
                     </p>
                     <p>
                       <strong>{actress.awards}</strong>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+
+        <hr />
+
+        <h1 className="text-center mt-2 mb-2">
+          <em>Hollywood Actors</em>
+        </h1>
+        {actors.map((actor, id) => {
+          return (
+            <div className="col-3 mb-3 mt-3" key={id}>
+              <div
+                className="card h-100"
+                style={{ backgroundColor: "#939FC6" }}
+              >
+                <img
+                  src={actor.image}
+                  className="card-img-top"
+                  alt={actor.name}
+                />
+                <div className="card-body text-center">
+                  <h5 className="card-title">{actor.name}</h5>
+                  <div className="card-text">
+                    <p>
+                      {actor.birth_year} - {actor.nationality}
+                    </p>
+                    <p>
+                      <em>{actor.biography}</em>
+                    </p>
+                    <p>
+                      <strong>{actor.awards}</strong>
                     </p>
                   </div>
                 </div>
