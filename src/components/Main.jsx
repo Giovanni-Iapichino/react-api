f597026323f91f185afe59187d581d0da9b18750
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-28 16:09:41 +0200

    add-actresses-card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 
 export default function Main() {
   const apiBaseUrl = "https://lanciweb.github.io/demo/api/actresses/";
+  const [actresses, setActresses] = useState([]);
 
-  axios.get(`${apiBaseUrl}`).then((res) => {
-    console.log(res.data);
-  });
+  useEffect(() => {
+    axios.get(`${apiBaseUrl}`).then((res) => {
+      console.log(res.data);
+      const results = res.data;
+      setActresses(results);
+    });
+  }, []);
 
-  const { results } = res.data;
-
-  return;
+  return (
+    <div className="container">
+      <div className="row">
+        {actresses.map((actress, id) => {
+          return (
+            <div className="col-4" key={id}>
+              <div className="card h-100">
+                <img
+                  src={actress.image}
+                  className="card-img-top"
+                  alt={actress.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{actress.name}</h5>
+                  <div className="card-text">
+                    <ul>
+                      <li>
+                        {actress.birth_year} - {actress.nationality}
+                      </li>
+                      <li>{actress.biography}</li>
+                      <li>{actress.awards}</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
